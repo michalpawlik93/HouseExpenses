8f446caeb1f66ac46cb48affa4e6e5458fb34aa6
Michal Pawlik <michal.pawlik@fenergo.com>
2022-12-12 21:30:38 +0100

    add ui folder

---
+++ ui/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App: React.FC<any> = () => {
+  return <>Tests</>
+}
+
+export default App
---
+++ ui/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/index.css'
+import App from './App'
+
+const rootElement = document.getElementById('root') as Element
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
